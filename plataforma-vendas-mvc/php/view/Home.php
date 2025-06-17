0f7e448cd5525a85f3be90fdf0d10b3302a0b75d
DanizDev <jose.daniel.alicrim9@hotmail.com>
2025-06-16 22:24:43 -0300

    Add files via upload
    Fazendo site, com php, javascript, e banco de dados, com algumas praticas de organização de pastas.

---
+++ plataforma-vendas-mvc/php/view/Home.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina Inicial</title>
+
+    <!-- estilização do cabeçalho -->
+     <link rel="stylesheet" type="text/css" href="./../../css/container-layout/cabecalho.css">
+     <link rel="stylesheet" type="text/css" href="./../../css/componentes/carrossel.css">
+
+    </head>
+<body>
+    <!-- cabeçalho -->
+     <?php
+        include '../view/Cabecalho.php';
+     ?>
+    <main class="container-principal">
+        <div class="carrossel">
+            <div class="slides">
+                <img src="./../../src\images/destaques/pastilha.jfif" alt="Pastilha de Freios Gol 1996-01">
+                <img src="./../../src\images/destaques/mocinetica.jfif" alt="Homocinética">
+                <img src="./../../src\images/destaques/parabrisa.jfif" alt="Parabrisa Palio 2003">
+                <img src="./../../src\images/destaques/gol.jfif" alt="Bico Injetor Gol 1996-01">
+            </div>
+            <button class="prev" onclick="mudarSlide(-1)"></button>
+            <button class="next" onclick="mudarSlide(1)"></button>
+        </div>
+    </main>    
+   <!-- rodapé -->
+    <script src="./../../js/carrossel.js"></script>
+</body>
+</html>
